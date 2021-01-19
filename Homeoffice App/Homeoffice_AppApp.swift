efd2155f4cfecc3ab629a1fcb2d6e5f41358d573
Lars91 <LarsBinner@web.de>
2021-01-19 17:19:52 +0100

    Initial Commit

---
+++ Homeoffice App/Homeoffice_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Homeoffice_AppApp.swift
+//  Homeoffice App
+//
+//  Created by Lars Binner on 19.01.21.
+//
+
+import SwiftUI
+
+@main
+struct Homeoffice_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
